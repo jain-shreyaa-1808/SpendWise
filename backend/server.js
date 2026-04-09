c338ae9edb306d2f94784b616b3cbde5663031c2
shreyaj2 <shreyaj2@cisco.com>
2026-04-09 16:59:58 +0530

    production ready

---
+++ backend/server.js
@@ -0,0 +1,38 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import { connectDB } from "./config/db.js";
+
+import userRouter from "./routes/userRoute.js";
+import incomeRouter from "./routes/incomeRoute.js";
+import expenseRouter from "./routes/expenseRoute.js";
+import dashboardRouter from "./routes/dashboardRoute.js";
+import path from "path";
+
+const app = express();
+const port = 4000;
+const _dirname = path.resolve();
+// MIDDLEWARES
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(_dirname, "/frontend/dist")));
+
+// DB
+connectDB();
+
+// ROUTES
+app.use("/api/user", userRouter);
+app.use("/api/income", incomeRouter);
+app.use("/api/expense", expenseRouter);
+app.use("/api/dashboard", dashboardRouter);
+
+app.get("/", (req, res) => {
+  res.send("API WORKING");
+});
+app.use((req, res) => {
+  res.sendFile(path.resolve("frontend/dist/index.html"));
+});
+app.listen(port, () => {
+  console.log(`Server Started on http://localhost:${port}`);
+});
